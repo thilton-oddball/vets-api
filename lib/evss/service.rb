7cb487e54d8d74ca8ee7c5f686234cee601c3872
Lihan Li <frankieteardrop@gmail.com>
2017-10-27 15:20:13 -0400

    log user used for call

--- lib/evss/service.rb
+++ lib/evss/service.rb
@@ -5,12 +5,15 @@ require 'evss/auth_headers'
 module EVSS
   class Service < Common::Client::Base
     SYSTEM_NAME = 'vets.gov'
+    include SentryLogging
 
     def headers_for_user(user)
       EVSS::AuthHeaders.new(user).to_h
     end
 
     def perform_with_user_headers(method, path, body, headers = {}, &block)
+      log_message_to_sentry('user used for call', :info, email: @current_user.email.delete('@'))
+
       perform(
         method,
         path,
