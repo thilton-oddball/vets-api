b844e7ce44f684d4d3d5fcbf187e82006fe79e39
Erik Lopez <Erik.Lopez@ablevets.com>
2018-12-20 13:26:18 -0500

    debugging ppms address weirdness

--- app/controllers/v0/facilities/ccp_controller.rb
+++ app/controllers/v0/facilities/ccp_controller.rb
@@ -8,6 +8,7 @@ class V0::Facilities::CcpController < FacilitiesController
     result = ppms.provider_info(params[:id])
     raise Common::Exceptions::RecordNotFound, params[:id] if result.nil?
     services = ppms.provider_services(params[:id])
+    Rails.logger.error(services[0]['CareSiteLocationAddress']) if services.present?
     caresites = ppms.provider_caresites(services[0]['CareSiteLocationAddress']) if services.present?
     result.add_caresite(caresites[0]) if caresites.present?
     render json: result, serializer: ProviderSerializer
