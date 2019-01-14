e887094f4bf652728213938e20fc22f6a23715bc
hpjaj <hmlevine@gmail.com>
2019-01-14 09:15:14 -0700

    Base mutation file prepped for adding mutations when time comes
    Mutations will follow the exact same pattern as queries.
    
    See this commit for the pattern:
    
    “Creates an extensible, organized, encapsulated query file structure”

---
+++ app/graphql/mutations/mutation.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+module Mutations
+  # The parent list of potential mutations that are made available to
+  # the GraphQL schema, VetsAPISchema.
+  #
+  # This list is comprised of individual lists (or manifests), each
+  # one containing their own assembly of available mutations.
+  #
+  class Mutation < Types::BaseObject
+  end
+end
